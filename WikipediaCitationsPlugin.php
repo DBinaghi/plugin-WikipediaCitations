a6337c1679bedb301f680e3e08c6b87d86e06d9e
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2020-08-03 15:51:26 +0200

    Added internationalization

--- WikipediaCitationsPlugin.php
+++ WikipediaCitationsPlugin.php
@@ -5,10 +5,11 @@ class WikipediaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
 	protected $_hooks = array(
 		'install',
 		'uninstall',
+		'initialize'
 	);
 
 	protected $_filters = array(
-		'item_citation',
+		'item_citation'
 	);
 
 	public function hookInstall()
@@ -21,7 +22,12 @@ class WikipediaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
 		$this->_uninstallOptions();
 	}
 	
-	public function filterItemCitation($citation,$args)
+	public function hookInitialize()
+	{
+		add_translation_source(dirname(__FILE__) . '/languages');
+	}
+
+	public function filterItemCitation($citation, $args)
 	{
 		$item = $args['item'];
 		
@@ -31,60 +37,59 @@ class WikipediaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
         if ($creators) {
             switch (count($creators)) {
                 case 1:
-                    $creator = preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0]) . ', ';
+                    $creator = preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0]);
                     break;
                 case 2:
                     /// Chicago-style item citation: two authors
-                    $creator = __('%1$s and %2$s', preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0]), preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[1])) . ', ';
+                    $creator = __('%1$s and %2$s', preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0]), preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[1]));
                     break;
                 case 3:
                     /// Chicago-style item citation: three authors
-                    $creator = __('%1$s, %2$s, and %3$s', preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0]), preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[1]), preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[2])) . ', ';
+                    $creator = __('%1$s, %2$s and %3$s', preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0]), preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[1]), preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[2]));
                     break;
                 default:
                     /// Chicago-style item citation: more than three authors
-                    $creator = __('%s et al.', preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0])) . ', ';
+                    $creator = __('%s et al.', preg_replace('/^(\w+),\s(\w+)/i', '$2 $1', $creators[0]));
             }
+			$creator .= ', ';
         }
         
         $title = metadata($item, 'display_title') ? '<em>' . metadata($item, 'display_title') . '</em>, ' : null;
 		
         $publishers = metadata($item, array('Dublin Core', 'Publisher'), array('all' => true));
-        // Strip formatting and remove empty creator elements.
+        // Strip formatting and remove empty publisher elements.
         $publishers = array_filter(array_map('strip_formatting', $publishers));
         if ($publishers) {
             switch (count($publishers)) {
                 case 1:
-                    $publisher = $publishers[0] . ', ';
+                    $publisher = $publishers[0];
                     break;
                 case 2:
                     /// Chicago-style item citation: two authors
-                    $publisher = __('%1$s and %2$s', $publishers[0], $publishers[1]) . ', ';
+                    $publisher = __('%1$s and %2$s', $publishers[0], $publishers[1]);
                     break;
                 case 3:
                     /// Chicago-style item citation: three authors
-                    $publisher = __('%1$s, %2$s, and %3$s', $publishers[0], $publishers[1], $publishers[2]) . ', ';
+                    $publisher = __('%1$s, %2$s and %3$s', $publishers[0], $publishers[1], $publishers[2]);
                     break;
                 default:
                     /// Chicago-style item citation: more than three authors
-                    $publisher = __('%s et al.', $publishers[0]) . ', ';
+                    $publisher = __('%s et al.', $publishers[0]);
 			}
+			$publisher .= ', ';
 		}
 
-        $date = metadata($item, array('Dublin Core', 'Date')) ? date('Y', metadata($item, array('Dublin Core', 'Date'))) . ', ' : null;
+        $date = metadata($item, array('Dublin Core', 'Date'));
+        $date = strlen($date) > 3 ? substr($date, 0, 4) . ', ' : ''; 
 
-        $extra = null;
+        $extra = '';
 		
 		if (element_exists('Item Type Metadata', 'ISBN')) {
-	        
-			$extra = metadata($item, array('Item Type Metadata', 'ISBN')) ? 'ISBN ' . metadata($item, array('Item Type Metadata', 'ISBN')) . ', ' : null; 
-	       
-        } 
-		
-		if (is_null($extra) && element_exists('Item Type Metadata', 'ISSN')) {
-			
-			$extra = metadata($item, array('Item Type Metadata', 'ISSN')) ? 'ISSN ' . metadata($item, array('Item Type Metadata', 'ISSN')) . ', ' : null; 
-			
+			$extra = metadata($item, array('Item Type Metadata', 'ISBN')) ? 'ISBN ' . metadata($item, array('Item Type Metadata', 'ISBN')) . ', ' : ''; 
+        } elseif (element_exists('Item Type Metadata', 'ISSN')) {
+			$extra = metadata($item, array('Item Type Metadata', 'ISSN')) ? 'ISSN ' . metadata($item, array('Item Type Metadata', 'ISSN')) . ', ' : ''; 
+		} elseif (strpos(metadata($item, array('Dublin Core', 'Identifier')), 'ISBN') !== false || strpos(metadata($item, array('Dublin Core', 'Identifier')), 'ISSN') !== false) {
+			$extra = metadata($item, array('Dublin Core', 'Identifier')) . ',';
 		}
         
         $accessed = format_date(time(), Zend_Date::DATE_LONG);
@@ -92,7 +97,7 @@ class WikipediaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
 		$url = WEB_ROOT . '/items/show/' . $item->id;
         $url = '<span class="citation-url">' . html_escape($url) . '</span>';
 
-		$citation = $creator . $title . $publisher . $date . $extra . ' ' . __('accessed %1$s, %2$s.', $accessed, $url);
+		$citation = $creator . $title . $publisher . $date . $extra . ' ' . __('accessed %1$s, %2$s', $accessed, $url);
 
         return $citation;
     }   
